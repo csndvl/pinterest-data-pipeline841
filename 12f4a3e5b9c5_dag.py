59511da992b32756f02938208c4b172cf5738afc
csndvl <chrisandoval10@gmail.com>
2023-12-03 14:46:13 +0000

    Uploads DAG file

---
+++ 12f4a3e5b9c5_dag.py
@@ -0,0 +1,45 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
+from datetime import datetime, timedelta 
+
+
+#Define params for Submit Run Operator
+notebook_task = {
+    'notebook_path': '/Users/chrisandoval10@gmail.com/Airflow',
+}
+
+
+#Define params for Run Now Operator
+notebook_params = {
+    "Variable":5
+}
+
+
+default_args = {
+    'owner': 'Christian Sandoval',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=2)
+}
+
+
+with DAG('12f4a3e5b9c5_dag',
+    # should be a datetime format
+    start_date=datetime(2023, 12, 3),
+    # check out possible intervals, should be a string
+    schedule_interval='@daily',
+    catchup=False,
+    default_args=default_args
+    ) as dag:
+
+
+    opr_submit_run = DatabricksSubmitRunOperator(
+        task_id='submit_run',
+        # the connection we set-up previously
+        databricks_conn_id='databricks_default',
+        existing_cluster_id='1108-162752-8okw8dgg',
+        notebook_task=notebook_task
+    )
+    opr_submit_run
